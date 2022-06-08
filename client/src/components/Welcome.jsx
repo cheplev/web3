0e0cef2ce9cb863b432c8ced2d12e474ddec6936
Cheplev Valeriy <vcheplev42@gmail.com>
2022-06-08 20:54:51 +0400

    navbar ready

---
+++ client/src/components/Welcome.jsx
@@ -0,0 +1,15 @@
+import { AiFillAlipayCircle } from "react-icons/ai";
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
+
+import { Loader } from './';
+
+const Welcome = () => {
+    return (
+        <div className="flex w-full justify-center items-center">
+
+        </div>
+    )
+}
+
+export default Welcome;
