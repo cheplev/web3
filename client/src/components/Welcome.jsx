a855bc0f9ea46bf86c301d492337fee65f7fe731
Cheplev Valeriy <vcheplev42@gmail.com>
2022-06-09 10:30:05 +0400

    content finished

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -4,16 +4,30 @@ import { BsInfoCircle } from "react-icons/bs";
 
 import { Loader } from './';
 
-const commonStyles = 'min-h-[70px] sm:px-0 px-2  flex justify-center items-center border-[0.5px] border-gray-400 text-white';
+const commonStyles = 'min-h-[70px] sm:px-0 px-2 flex justify-center items-center border-[0.5px] border-gray-400 text-white';
+
+const Input = ({ placeholder, name, type, value, handleChange}) => (
+    <input
+        placeholder={placeholder}
+        type={type}
+        step="0.0001"
+        value={value}
+        onChange={(e) => handleChange(e, name)}
+        className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+    />
+)
 
 const Welcome = () => {
     const connectWallet = () => {
         return
     }
+     const handleSUBMIT = () => {
+        return
+    }
     return (
         <div className="flex w-full justify-center items-center">
-            <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-                <div className="flex flex-1 justify-start flex-col md:mr-10">
+            <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+                <div className="flex flex-1 justify-start flex-col mf:mr-10">
                     <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
                         Send Crypto <br /> across the world
                     </h1>
@@ -47,7 +61,7 @@ const Welcome = () => {
                     </div>
                 </div>
 
-                <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10"> 
+                <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10"> 
                     <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
                         <div className="flex justify-between flex-col w-full h-full">
                             <div className="flex justify-between items-start">
@@ -58,7 +72,7 @@ const Welcome = () => {
                             </div>
                             <div>
                                 <p className="text-white font-light text-sm">
-                                    0xasdasda45tfrenoi238
+                                    0xb794f5ea0b...79268
                                 </p>
                                 <p className="text-white font-semibold text-lg mt-1">
                                     Ethereum
@@ -66,6 +80,26 @@ const Welcome = () => {
                             </div>
                         </div>
                     </div>
+
+                    <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+                        <Input placeholder="Address To" name="addressTo" type="text" handleChange={() => { } }/>
+                        <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={() => { } }/>
+                        <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={() => { } }/>
+                        <Input placeholder="Enter Message" name="message" type="text" handleChange={() => { } }/>
+                        <div className="h-[1px] w-full bg-gray-400 my-2"/>
+
+                        {false ? (
+                            <Loader />
+                        ) : (
+                            <button
+                                type="button"
+                                onClick={handleSUBMIT}
+                                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer"
+                            > 
+                            Send Now
+                            </button>)}
+                    </div>
+
                 </div>
 
             </div>
