141c875b09184b18d9acabccce74d330b8abd6ff
Cheplev Valeriy <vcheplev42@gmail.com>
2022-06-09 11:01:52 +0400

    deploy add

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.0;
 
 contract Transactions {
-    uint256 trancsactionsCounter;
+    uint256 trancsactionsCount;
 
     event Transfer(address from, address reciever, uint amount, string message, uint256 timestamp, string keyword);
 
@@ -17,4 +17,19 @@ contract Transactions {
     }
 
     TransferStruct[] transactions;
+
+    function addToBlockchain(address payable receiver, uint amount, string memory message, string memory keyword) public {
+        trancsactionsCount++;
+        transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
+
+        emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
+    }     
+
+    function getAllTransactions() public view returns(TransferStruct[] memory) {
+        return transactions;
+    }
+
+    function getTransactionsCount() public view returns(uint256) {
+        return trancsactionsCount;
+    }
 }
